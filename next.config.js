088ec68c05c501c00714cf900342825897c5efad
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-09-13 14:59:13 -0500

    initial

---
+++ next.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  //reactStrictMode: false,
+  //generateEtags: false,
+  reactStrictMode: false,
+  analyticsId: '7vsHWbYwcKpA3lFTng2Qe9iNzE7',
+  env: {
+    siteTitle: "Dark Horse SC Minting Dapp",
+    contractAddress: "0xA14ac692b9CD258e45A89Ea6Aa307011f68B8694",
+    infuraID: "045e9c65387a4200aa8559c347fa19a4",
+    defaultGas: 125000,//75000,
+    mintType : "Pre-Sale", //Pre-Sale Public
+    enforceWhitelist : true,
+    ethValue : "0.1",
+    ethWLValue : "0.0",
+    debug: false,
+    network: 'mainnet',
+    chainId: 1,
+    pageHeader : 'The Dark Horse SC',
+    pageText : 'We Like The Odds',
+    //mintType: "Whitelist",
+    blockExplorerURL: 'https://.etherscan.io/',
+    www: 'https://DarkHorseSportsClub.com/',
+    mainWWW: 'https://DarkHorseSportsClub.com/',
+    title: 'The Dark Horse SC',
+    links : {
+      opensea : "https://opensea.io/collection/Dark-Horse-SC",
+      discord : "https://discord.com/invite/darkhorsesportsclub",
+      twitter : "https://twitter.com/DHSC_NFT"
+    },
+    maxMint: 9999,
+    maxMintCount: 50,
+    messagehash: '0x865f293da4a629f00bd8072be6115031c608fa4a13b819fd087811752fc8bb90',
+    signatureBytes: '0x4b7715f34823936b045d1e5ca6c295b7f5ecff3d0c158118c71441ed217c3ddb026617ce49dc15781bdb25c8c34ed4a1f4d68df6080f6dd0004511cf05032bf01c',
+    messagehash2: '0x417ebaa8de17c9a73acf7cf3f6c998ca2dd1e993276cc9dd37dfb7fb0cb197a4',
+    signatureBytes2: '0xe191166afd511a08818c2f55906e65db02dfe2366708885e92a2e54cef7ac2673e5dfe70c2d0d63a337c7792906a623f3aff0506aa2c5de6325e2d689bab95731b'
+  },
+  images: {
+    domains: ['static.wixstatic.com']
+  }
+}
